a0bf96cbe3730b12940552b93490964f193fd670
ralfz123 <ralf_zonneveld@hotmail.com>
2021-02-02 18:20:29 +0100

    Added interaction for new feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
+// 1. Data fetch (getdata)
+// 2. Dynamic data via virtualDOM
+// 3. favoritesList
+// 4. Like adds item (cat+joke) to favList
+// 5. Localstorage
+
+// API #1 FETCH
 let endpoint = 'https://api.thecatapi.com/v1/images/search';
 getData(endpoint);
 
@@ -9,27 +16,42 @@ function getData(url) {
     .then((data) => (image.src = data[0].url));
 }
 
-// -------
+// API #2 FETCH
+let endpointTwo = 'https://official-joke-api.appspot.com/jokes/random';
 
-getDataTwo('https://official-joke-api.appspot.com/jokes/random');
+getDataTwo(endpointTwo);
 
 function getDataTwo(url) {
   let joke = document.getElementById('joke');
   let punchline = document.getElementById('punchline');
-  let h2ID = document.getElementById('id');
-  
+  let id = document.getElementById('joke-id');
 
   fetch(url)
     .then((response) => response.json())
     .then(
-      (data) => ((joke.innerHTML = data.setup),
-       (punchline.innerHTML = data.punchline)
-       (h2ID.innerHTML = 'id: ' + data.id))
+      (data) => (
+        (joke.innerHTML = data.setup),
+        (punchline.innerHTML = data.punchline)(
+          (id.innerHTML = 'id: ' + data.id)
+        )
+      )
     );
 }
 
-// -------
-
+// Reload page
 function reloadPage() {
   location.reload();
+}
+
+// Liked button
+let likeButton = document.getElementById('likeButton');
+
+likeButton.addEventListener('click', check);
+
+function check() {
+  const newDiv = document.createElement('div');
+  const newContent = document.createTextNode('Liked: {cat-item} + {joke-item}');
+  newDiv.appendChild(newContent);
+  const currentDiv = document.getElementById('likeArea');
+  currentDiv.appendChild(newDiv);
 }
